a5ec6af0e22085c4c3723b56e1003ed2007ad2e8
Jan-Willem Wisgerhof <jwwisgerhof@gmail.com>
2017-07-11 16:37:11 +1000

    Change NewRelic to use the archive instead of /release

--- fs/newrelic-install.sh
+++ fs/newrelic-install.sh
@@ -6,9 +6,9 @@ export NR_INSTALL_SILENT=1
 export NR_INSTALL_PHPLIST=/usr/bin
 export NR_INSTALL_KEY="${NEWRELIC_LICENSE}"
 
-NEWRELIC_VERSION="7.3.1.197"
+NEWRELIC_VERSION="7.4.0.198"
 cd /tmp
-wget -q https://download.newrelic.com/php_agent/release/newrelic-php5-${NEWRELIC_VERSION}-linux-musl.tar.gz
+wget -q https://download.newrelic.com/php_agent/archive/${NEWRELIC_VERSION}/newrelic-php5-${NEWRELIC_VERSION}-linux-musl.tar.gz
 tar -zxf newrelic-php5-${NEWRELIC_VERSION}-linux-musl.tar.gz
 cd newrelic-php5-${NEWRELIC_VERSION}-linux-musl
 ./newrelic-install install
